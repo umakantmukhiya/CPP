6f9f9b1b8755e0523b3346a9b96013b05ed566fe
umakantmukhiya <umakant.1mu@gmail.com>
2021-06-05 21:03:04 +0530

    only_zero is false in cpp

---
+++ only_zero_is_false.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    if(-4){
+        cout << "Only zero(0) is false ";
+    }
+
+    return 0;
+}
--- typecasting.cpp
+++ typecasting.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(){
     int var1 = 1500000000;
-    int var2 = (static_cast)/10E9;
+    int var2 = (static_cast<double>(var1) * 10)/10E8;
+    cout << "Answer is " << var2;
+
+    return 0;
 }
